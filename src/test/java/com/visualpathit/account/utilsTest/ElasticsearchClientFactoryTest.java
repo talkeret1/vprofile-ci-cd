d1864fc278895faa0cd1d61344b7c7bfd9500891
talkeret1 <talkeret@gmail.com>
2026-06-04 13:26:16 +0300

    #21

---
+++ src/test/java/com/visualpathit/account/utilsTest/ElasticsearchClientFactoryTest.java
@@ -0,0 +1,81 @@
+package com.visualpathit.account.utilsTest;
+
+import com.visualpathit.account.beans.Components;
+import com.visualpathit.account.utils.ElasticsearchClientFactory;
+import org.junit.jupiter.api.Test;
+import org.opensearch.client.RestHighLevelClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ElasticsearchClientFactoryTest {
+
+    @Test
+    void createClientShouldReturnClientWhenConfigurationIsValid() {
+
+        Components components = mock(Components.class);
+
+        when(components.getElasticsearchHost()).thenReturn("localhost");
+        when(components.getElasticsearchPort()).thenReturn("9200");
+        when(components.getElasticsearchScheme()).thenReturn("http");
+
+        ElasticsearchClientFactory factory = new ElasticsearchClientFactory(components);
+
+        RestHighLevelClient client = factory.createClient();
+
+        assertNotNull(client);
+
+        try {
+            client.close();
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void createClientShouldThrowExceptionWhenHostIsNull() {
+
+        Components components = mock(Components.class);
+
+        when(components.getElasticsearchHost()).thenReturn(null);
+        when(components.getElasticsearchPort()).thenReturn("9200");
+        when(components.getElasticsearchScheme()).thenReturn("http");
+
+        ElasticsearchClientFactory factory = new ElasticsearchClientFactory(components);
+
+        assertThrows(
+                IllegalStateException.class,
+                factory::createClient);
+    }
+
+    @Test
+    void createClientShouldThrowExceptionWhenPortIsBlank() {
+
+        Components components = mock(Components.class);
+
+        when(components.getElasticsearchHost()).thenReturn("localhost");
+        when(components.getElasticsearchPort()).thenReturn(" ");
+        when(components.getElasticsearchScheme()).thenReturn("http");
+
+        ElasticsearchClientFactory factory = new ElasticsearchClientFactory(components);
+
+        assertThrows(
+                IllegalStateException.class,
+                factory::createClient);
+    }
+
+    @Test
+    void createClientShouldThrowExceptionWhenSchemeIsBlank() {
+
+        Components components = mock(Components.class);
+
+        when(components.getElasticsearchHost()).thenReturn("localhost");
+        when(components.getElasticsearchPort()).thenReturn("9200");
+        when(components.getElasticsearchScheme()).thenReturn("");
+
+        ElasticsearchClientFactory factory = new ElasticsearchClientFactory(components);
+
+        assertThrows(
+                IllegalStateException.class,
+                factory::createClient);
+    }
+}
